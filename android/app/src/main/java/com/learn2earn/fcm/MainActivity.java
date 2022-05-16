cdac9ce26b090ca7d188288b4bef454f09567cb6
The Dark Eye <thedarkeye.exe@gmail.com>
2022-05-16 11:35:13 +0500

    files added

---
+++ android/app/src/main/java/com/learn2earn/fcm/MainActivity.java
@@ -0,0 +1,5 @@
+package com.learn2earn.fcm;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import {
+  PushNotifications,
+  PushNotificationSchema,
+} from '@capacitor/push-notifications';
+import { FCM } from '@capacitor-community/fcm';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  session: any;
 
-  constructor() {}
+  notifications: PushNotificationSchema[] = [];
 
+  topicName = 'super-awesome-topic';
+  remoteToken: string;
+
+  constructor(private platform: Platform, private zone: NgZone) {}
+
+  ngOnInit() {
+    PushNotifications.addListener('registration', (data) => {
+      // alert(JSON.stringify(data));
+      console.log(data);
+    });
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        console.log('notification ' + JSON.stringify(notification));
+        this.zone.run(() => {
+          this.notifications.push(notification);
+        });
+      }
+    );
+    PushNotifications.requestPermissions().then((response) =>
+      PushNotifications.register().then(() => alert(`registered for push`))
+    );
+  }
+
+  // Subscribed to fcm
+  subscribeTo() {
+    PushNotifications.register()
+      .then((_) => {
+        FCM.subscribeTo({ topic: this.topicName })
+          .then((r) => alert(`subscribed to topic ${this.topicName}`))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => alert(JSON.stringify(err)));
+  }
+
+  unsubscribeFrom() {
+    FCM.unsubscribeFrom({ topic: this.topicName })
+      .then((r) => alert(`unsubscribed from topic ${this.topicName}`))
+      .catch((err) => console.log(err));
+
+    if (this.platform.is('android')) {
+      FCM.deleteInstance();
+    }
+  }
+
+  getToken() {
+    FCM.getToken()
+      .then((result) => {
+        this.remoteToken = result.token;
+      })
+      .catch((err) => console.log(err));
+  }
 }
