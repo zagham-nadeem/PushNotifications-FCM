f940780258cdfd259f309370c42bd780936380f9
The Dark Eye <thedarkeye.exe@gmail.com>
2022-05-16 09:37:44 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learn2earn.fcm',
+  appName: 'fcmNotifications',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
